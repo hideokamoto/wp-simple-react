44c5131d454cebcc18577b339b02d3e151ec45cd
hideokamoto <hide.okamoto@digitalcube.jp>
2016-04-02 00:57:28 +0900

    add webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,30 @@
+path = require('path');
+const webpack = require('webpack');
+const babel = require('babel-core');
+
+module.exports = {
+ devtool: 'eval',
+ entry: './src/app.js',
+ output: {
+   path: path.join(__dirname, 'build'),
+   filename: 'bundle.js',
+   publicPath: '/static/',
+ },
+ module: {
+   loaders: [
+	 {
+	   test: /\.js$/,
+	   loaders: ['babel']
+	 }
+   ],
+ },
+};
+
+/*
 var webpack = require("webpack");
 new webpack.DefinePlugin({
   "process.env": {
     NODE_ENV: JSON.stringify("production")
   }
 });
+*/

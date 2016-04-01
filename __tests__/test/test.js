81e5d72664dbc1265bd26d1899ed5cd8458e8a74
hideokamoto <hide.okamoto@digitalcube.jp>
2016-04-02 01:33:26 +0900

    init create tests

---
+++ __tests__/test/test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+describe('Jest Test', () => {
+
+  it('is it works ?', () => {
+
+    expect(true).toEqual(true);
+  });
+
+});

8383ec3cac41afcb55183329ca227ff4cc9da1b6
hideokamoto <hide.okamoto@digitalcube.jp>
2016-04-02 01:26:38 +0900

    updave packages( run webpack server )

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
 path = require('path');
 const webpack = require('webpack');
-const babel = require('babel-core');
+const babel = require('babel-loader');
 
 module.exports = {
- devtool: 'eval',
- entry: './src/app.js',
- output: {
-   path: path.join(__dirname, 'build'),
-   filename: 'bundle.js',
-   publicPath: '/static/',
- },
- module: {
-   loaders: [
-	 {
-	   test: /\.js$/,
-	   loaders: ['babel']
-	 }
-   ],
- },
+	devtool: 'eval',
+	entry: './src/app.js',
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: 'bundle.js',
+		publicPath: '/build/'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loaders: ['babel']
+			}
+		],
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
 };
 
 /*
-var webpack = require("webpack");
 new webpack.DefinePlugin({
   "process.env": {
     NODE_ENV: JSON.stringify("production")

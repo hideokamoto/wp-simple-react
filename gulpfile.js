163364677cf7d770e28cd0cb70f083b422a146d3
hideokamoto <hide.okamoto@digitalcube.jp>
2016-04-02 00:13:59 +0900

    init gulp file ( only webserver )

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+
+gulp.task('webserver', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      host: '127.0.0.1',
+      livereload: true
+    })
+  );
+});
+
+gulp.task('default', ['webserver']);

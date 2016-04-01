55fd026d71c41ee3f225bdafd8a107beacc192a8
hideokamoto <hide.okamoto@digitalcube.jp>
2016-04-02 01:43:43 +0900

    webpack test server

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 	</head>
 	<body>
 		<div id="app"></div>
-		<script src="http://rest-api.dev/wp-content/themes/simple-react/build/bundle.js"></script>
+		<script src="http://rest-api.dev/wp-content/themes/simple-react/build/app.js"></script>
 	</body>
 </html>
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 path = require('path');
 const webpack = require('webpack');
 const babel = require('babel-loader');
+const glob = require('glob');
 
 module.exports = {
 	devtool: 'eval',
-	entry: './src/app.js',
+	entry: {
+		app: './src/app.js',
+		test: glob.sync("./__tests__/**/tests*.js"),
+	},
 	output: {
 		path: path.join(__dirname, 'build'),
-		filename: 'bundle.js',
+		filename: '[name].js',
 		publicPath: '/build/'
 	},
 	module: {
@@ -16,12 +20,31 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /(node_modules)/,
 				loaders: ['babel']
-			}
+			},
+			{
+				test: /tests\.js$/,
+				exclude: /(node_modules)/,
+				loaders: ['babel']
+			},
 		],
 	},
 	resolve: {
 		extensions: ['', '.js', '.jsx']
 	},
+	devServer: {
+		// Document Root
+		contentBase: "./",
+		// 動作ポート指定
+		port: 8080,
+		// hotモード有効化
+		hot: true,
+		// これがないと、ブラウザで
+		inline: true,
+	},
+	plugins: [
+		// hotモードに必要なプラグイン
+		new webpack.HotModuleReplacementPlugin(),
+	],
 };
 
 /*

8e8cd3d962cf216501409b54414d9c43c40e25e8
hideokamoto <hide.okamoto@digitalcube.jp>
2016-04-02 21:43:22 +0900

    rename

--- index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE HTML>
-<html>
-	<head>
-	</head>
-	<body>
-		<div id="app"></div>
-		<script src="http://rest-api.dev/wp-content/themes/simple-react/build/app.js"></script>
-	</body>
-</html>
